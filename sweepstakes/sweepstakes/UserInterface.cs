dba85e026d38943816ddeb8e7034c8a004b1f7b9
manzilat <amtulnoorq@yahoo.com>
2018-10-11 16:53:50 -0500

    use a while loop to get add cotestant,displaycontestantans,pickwinner

--- sweepstakes/sweepstakes/UserInterface.cs
+++ sweepstakes/sweepstakes/UserInterface.cs
@@ -8,5 +8,33 @@ namespace sweepstakes
 {
     class UserInterface
     {
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            Console.Write("Please Enter Selector:   ");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+        public static string GetContestantFirstName()
+        {
+            string userInput = GetUserInput("Please enter your first name: ");
+            return userInput;
+        }
+        public static string GetContestantLastName()
+        {
+            string userInput = GetUserInput("Please enter your last name: ");
+            return userInput;
+        }
+        public static string GetContestantEmailAddress()
+        {
+            string userInput = GetUserInput("Please enter your email address: ");
+            return userInput;
+        }
+        public static Guid SetRegistrationNumber()
+        {
+            Guid newGuid;
+            newGuid = Guid.NewGuid();
+            return newGuid;
+        }
     }
 }
